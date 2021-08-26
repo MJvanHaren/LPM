753d43bec3dbc55e23d2a40e265eff5b68c87e6e
Max van Haren <maxvanharen@live.nl>
2021-08-26 13:59:24 +0200

    Added closed loop+example and commented open loop

--- LPMClosedLoopArbitrary.m
+++ LPMClosedLoopArbitrary.m
@@ -1,9 +1,10 @@
-function [G_LPM, T_LPM] = LPMOpenLoopArbitrary(u,y,n,R)
+function [G_LPM, T_LPM] = LPMClosedLoopArbitrary(u,y,r,n,R)
 % This script will calculate a local polynomial model for the given in- and output of a system.
 % The system is assumed to be SISO and in open loop.
 % Inputs:
 %     u : Input to (open loop) plant in time domain
 %     y : Output of plant to given input signal u
+%     r : Reference to the plant (not disturbance! see Pintelon 2012 Fig. 7-4)
 %     n : Window size (left and right) for frequency bin k
 %     R : Degree of polynomial, e.g. G(omega_k+r) = G(omega)+sum_s=1^R g_s(k)*r^s
 % Outputs:
@@ -20,13 +21,16 @@ end
 Np = length(u); % signal length
 N = floor(Np/2); % amount of samples up to nyquist
 
-thetaHat = zeros(N,2*(R+1)); % Pintelon2012 (7-6)
+thetaHat = zeros(2,2*(R+1),N); % Pintelon2012 (7-6)
 K1 = @(r) (r*ones(R+1,1)).^((0:R)'); % basis for LPM
 
 Uf=fft(u)/sqrt(Np);
 Yf=fft(y)/sqrt(Np);
-Y = Yf(1:N)'; % up to nyquist frequency
-U = Uf(1:N)';
+Rf=fft(r)/sqrt(Np);
+Yk = Yf(1:N)'; % up to nyquist frequency
+Uk = Uf(1:N)';
+Zk = [Yk;Uk]; % Pintelon 2012 (7-48)
+Rk = Rf(1:N)';
 
 % Enzo's way
 % Y(n + 1 : N + 2 * n ) = Yf(1 : N + n);
@@ -48,7 +52,7 @@ for k = 1:N
     end
     Kn = zeros(2*(R+1),2*n+1); % reset Kn for every iteration k
     for i = 1:2*n+1
-        Kn(:,i) = [K1(r(i)).*U(k+r(i)); K1(r(i))]; 
+        Kn(:,i) = [K1(r(i)).*Rk(k+r(i)); K1(r(i))]; 
     end
     
     % scaling, see Pintelon2012 (7-25)
@@ -59,13 +63,14 @@ for k = 1:N
     
     Kn = Dscale\Kn;
     
-    [Uk,Sk,Vk] = svd(Kn'); % better computational feasability Pintelon 2012 (7-24)
-    thetaHat(k,:) = Y(k+r)*Uk/Sk'*Vk';
-    thetaHat(k,:) = thetaHat(k,:)/Dscale;
-    Vhn = Y(k+r)'-thetaHat(k,:)*Kn; % Not used right now, Pintelon (7-11)
+    [U_k,S_k,V_k] = svd(Kn'); % better computational feasability Pintelon 2012 (7-24)
+    thetaHat(:,:,k) = Zk(:,k+r)*U_k/S_k'*V_k';
+    thetaHat(:,:,k) = thetaHat(:,:,k)/Dscale;
 end
-G_LPM = thetaHat(:,1); % calculate LPM estimate of system
-T_LPM = thetaHat(:,R+2); % calculate LPM estimate transient contribution of system 
+Grz_LPM = squeeze(thetaHat(:,1,:)); % calculate LPM estimate of system
+Trz_LPM = squeeze(thetaHat(:,R+2,:)); % calculate LPM estimate transient contribution of system 
+G_LPM = Grz_LPM(1,:)./Grz_LPM(2,:);
+T_LPM = Trz_LPM(1,:)./Trz_LPM(2,:);
 
 % Enzo's way
 % G = thetaHat(n+1:end,1); 
--- LPMExample_ArbitrarySignals_ClosedLoop.m
+++ LPMExample_ArbitrarySignals_ClosedLoop.m
@@ -11,9 +11,8 @@ Ts = 1/250;
 Omega2 = 4*Omega1; % rad/s
 P = 5.6e3*(0.9*s^2+2*s+5.4e4)/(1.1*0.9*s^4+(1.1+0.9)*2*s^3+(1.1+0.9)*5.4e4*s^2);
 C = 5*(1/(10*2*pi)*s+1)/(1/(40*2*pi)*s+1);
-G0s = C/(1+P*C);
-G0 = c2d(G0s,Ts,'tustin');
-ssG0 = ss(G0);
+P = c2d(P,Ts,'tustin');
+C = c2d(C,Ts,'tustin');
 
 n = 4; % window size left and right
 R=2; % max. degree of polynomial in LPM
@@ -26,36 +25,37 @@ A = ones(N,1); % amplitude distribution
 t = (0:Ts:(Np-1)*Ts)';
 
 % custom multisine
-u = zeros(Np,1);
+r = zeros(Np,1);
 for k = 1:N
-   u = u+A(k)*sin(2*pi*f(k)*t+rand*2*pi);
+   r = r+A(k)*sin(2*pi*f(k)*t+rand*2*pi);
 end
 
 % white noise
-% u = randn(Np,1);
+% r = randn(Np,1);
 %% simulate and LPM/ETFE
-% y = lsim(G0,u,t)+0.001*randn(Np,1); % with output noise
-y = lsim(G0,u,t); % noiseless
-G_ETFE = etfe([y u],100,N);
-[G_LPM,T_LPM] = LPMOpenLoopArbitrary(u,y,n,R);
+y = lsim(P*C/(1+P*C),r,t); % Pintelon 2012 Figure 7-4!
+u = lsim(C/(1+P*C),r,t); % Pintelon 2012 Figure 7-4!
+
+P_ETFE = etfe([y u],100,N);
+[P_LPM,T_LPM] = LPMClosedLoopArbitrary(u,y,r,n,R);
 %% plotting
 figure(1); clf;
 opts = bodeoptions;
 opts.FreqUnits = 'Hz';
 opts.xlim = [1e-1 125];
-bodemag(G0,opts); hold on;
-semilogx(f,20*log10(abs(G_LPM)),'Color',c2); hold on %-(n-1)*Ts or -n*Ts is perfect! -> fix in freq window?
+bodemag(P,opts); hold on;
+semilogx(f,20*log10(abs(P_LPM)),'Color',c2); hold on %-(n-1)*Ts or -n*Ts is perfect! -> fix in freq window?
 semilogx(f,20*log10(abs(T_LPM(1:length(f)))),'Color',c3);
-semilogx(G_ETFE.Frequency*128/pi,20*log10(squeeze(abs(G_ETFE.ResponseData))),'Color',c4);
+semilogx(P_ETFE.Frequency*128/pi,20*log10(squeeze(abs(P_ETFE.ResponseData))),'Color',c4);
 legend('True plant','Estimated plant','Estimated transient plant','ETFE')
 
 figure(2); clf;
-[magG,~,~] = bode(G0,f*2*pi);
+[magG,~,~] = bode(P,f*2*pi);
 absG = (squeeze(magG));
-semilogx(f,mag2db(abs(abs(G_LPM)-absG)),'Color',c2); hold on 
-[magG,~,~] = bode(G0,G_ETFE.Frequency*128*2);
+semilogx(f,mag2db(abs(abs(P_LPM')-absG)),'Color',c2); hold on 
+[magG,~,~] = bode(P,P_ETFE.Frequency*128*2);
 absG = (squeeze(magG));
-semilogx(G_ETFE.Frequency*128/pi,mag2db(abs(squeeze(abs(G_ETFE.ResponseData))-absG)),'Color',c4);
+semilogx(P_ETFE.Frequency*128/pi,mag2db(abs(squeeze(abs(P_ETFE.ResponseData))-absG)),'Color',c4);
 xlabel('Frequency [Hz]'); xlim([f(1) f(end)]);
 ylabel('Estimation Error [dB]');
 legend('LPM','ETFE','location','best')
